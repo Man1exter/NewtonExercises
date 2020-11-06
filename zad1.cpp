671233fa761a9ba256f604097a4409b6caae06b4
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-06 19:43:03 +0100

    complete first exercises to sum the line in plasx

--- zad1.cpp
+++ zad1.cpp
@@ -4,7 +4,23 @@
 using namespace std;
 
 
+
 void obliczOdleglosc(){
+
+    int x1,x2,y1,y2,d;
+
+ cout << "podaj wartość x1: " << endl;
+cin >> x1;
+    cout << "podaj wartość x2: " << endl;
+cin >> x2;
+    cout << "podaj wartość y1: " << endl;
+cin >> y1;
+    cout << "podaj wartość y2: " << endl;
+cin>> y2;
+
+d = sqrt(pow(x2-x1, 2.0)+ pow(y2-y1,2.0));
+
+cout << "odległość wynosi: " << d << endl;
 
 }
 
@@ -13,6 +29,7 @@ int main()
 {
 
 obliczOdleglosc();
+
 
     return 0;
 }
