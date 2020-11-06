c67971f74f610546c15742710a6d50ac6e50a4d9
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-06 20:01:29 +0100

    save changes in file second on task two to done

--- zad2.cpp
+++ zad2.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
+
+
+
+void czytajDane(){
+
+    int a,b,c;
+
+    cout << "Wprowadz dane a: ";
+    cin>> a;
+
+    while(a < 0){
+
+        cout << "liczby nie mogą być mniejsze od 0!" << endl;
+        
+        return;         // zabezpieczenie przed wpisaniem liczby mniejszej od zera! 
+    }
+
+    cout << "Wprowadz dane b: ";
+    cin>> b;
+    cout << "Wprowadz dane c: ";
+    cin>> c;
+
+
+    cout << "Twoje  dane  to  a:  " << a << endl;
+    cout << "Twoje  dane  to  b:  " << b << endl;
+    cout << "Twoje  dane  to  c:  " << c << endl;
+}
+
 
 int main()
 {
 
-
+czytajDane();
 
 
 
