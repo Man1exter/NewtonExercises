021dc39e05bdf30faf25596ff2cad3839ddc6ca2
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-06 18:54:42 +0100

    start with static function in function

--- zad1.cpp
+++ zad1.cpp
@@ -1,37 +1,18 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 
 void obliczOdleglosc(){
 
-d*d = (x1-x2) * (x1-x2) + (y1-y2) * (y1-y2);
-
 }
-
 
 
 int main()
 {
 
-int x1,x2,y1,y2;
-int d; // wynik
-
-cout << "podaj parametr x1" << endl;
-cin >> x1;
-cout << "podaj parametr x2" << endl;
-cin >> x2;
-cout << "podaj parametr y1" << endl;
-cin >> y1;
-cout << "podaj parametr x2" << endl;
-cin >> y2;
-
-cout << obliczOdleglosc(x1,x2,y1,y2) << endl;
-
-
-
-
-
+obliczOdleglosc();
 
     return 0;
 }
