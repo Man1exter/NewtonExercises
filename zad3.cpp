2bb5000a73060fe4c52525180c4fc82729a04d33
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-07 11:04:32 +0100

    save changes with exercises three to complete all

--- zad3.cpp
+++ zad3.cpp
@@ -7,9 +7,11 @@ using namespace std;
 long long silnia(int n)                 // long long ---- 92235678234 do 92234567234 ------
 {
  if( n < 2 )
-        return 1;                          //silnia z 0 i 1 wynosi 1
 
-return n * silnia(n-1);                
+ return 1;                          //silnia z 0!= 1 
+
+return n * silnia(n-1);   
+
 }
 
 
@@ -18,13 +20,14 @@ int main()
 
  int n;
  
- cout << "Podaj liczbę: "<< endl;
+ cout << "Podaj wartość n: "<< endl;
  cin >> n;
+
  cout << "n! = " << silnia(n) << endl;
 
 
  return 0;
- 
+
 }
 
 // silnia podstawia za n liczby od 1 do n
