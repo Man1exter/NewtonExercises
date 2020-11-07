7cc944bd4d3087ced3c9c661332b56f07acf27c1
Mariusz Perzynski <m.perzyk@interia.pl>
2020-11-07 10:34:40 +0100

    rekurencin arrow is done to use function main

--- zad3.cpp
+++ zad3.cpp
@@ -1,14 +1,60 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
+
+
+
+
+void podajDoIlu(){
+int n;
+
+cout << "Podaj numer silni do obliczenia: " << endl;
+cin >> n;
+}
+
+
+
+
+void obliczenia(){
+int n;
+
+if(n >= 1 ){
+
+n = n * (n-1);
+
+} else {
+
+    cout << "Wartość musi być większa lub równa 1" << endl;
+    return;
+}
+cout << "wynik to: " << n << endl;
+}
+
+
+
+
+
+void ostateczny(){
+    int n;
+
+    cout << "ostateczny wynik to: " << n << endl;
+}
+
 
 int main()
 {
 
-
-
+podajDoIlu();
+obliczenia();
+ostateczny();
 
 
 
     return 0;
 }
+
+// silnia podstawia za n liczby od 1 do n
+
+// 0!=1,
+// n!=n*(n-1)!, gdzie n >= 1
